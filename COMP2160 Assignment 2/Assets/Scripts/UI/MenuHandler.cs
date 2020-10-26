71f9b8ebceeb8baae57fd8f9de0263922a34c2c1
Matthew Evangelidis <54091051+Notintheslightest@users.noreply.github.com>
2020-10-26 22:11:19 +1100

    Added a pause menu, MenuHandler script, and pausing functionality.

---
+++ COMP2160 Assignment 2/Assets/Scripts/UI/MenuHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject gameOverMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            if (!pauseMenu.activeInHierarchy)
+            {
+                PauseGame();
+            }
+
+            else if (pauseMenu.activeInHierarchy)
+            {
+                UnpauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    }
+
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+}
