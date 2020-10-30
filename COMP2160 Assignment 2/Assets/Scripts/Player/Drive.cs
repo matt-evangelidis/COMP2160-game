f72eb10dbec87ca37f0a137ce7cead81ab146295
Cody's Main Laptop <codster761@gmail.com>
2020-10-31 03:44:35 +1100

    Implemented hill sliding

--- COMP2160 Assignment 2/Assets/Scripts/Player/Drive.cs
+++ COMP2160 Assignment 2/Assets/Scripts/Player/Drive.cs
@@ -6,55 +6,122 @@ using UnityEngine;
 public class Drive : MonoBehaviour
 {
     public float speed = 10f;
-    public float rotation = 90f; //in degrees
+    public float rotation = 90f; // amount of torque applied when turning
 	
-	private float angle = 0f;
+	private Rigidbody rb;
 	
-	private Rigidbody rigidbody;
+	private float slideScale; // A scale value for how much the car slides downhill based on its rotation.
+	
+	private bool onGround;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = gameObject.GetComponent<Rigidbody>();
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-		//Debug.Log(Input.GetAxis("Forward"));
     }
 	
 	void FixedUpdate()
-	{	
-		if(Input.GetButton("Forward"))
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, -transform.up, out hit, 0.35f))
 		{
-			rigidbody.AddRelativeForce(Vector3.forward * speed * Time.deltaTime);
-			
-			if(Input.GetButton("Left"))
-			{
-				rigidbody.AddRelativeTorque(Vector3.up * -rotation * Mathf.Max(0.6f, Input.GetAxis("Forward")) * Time.deltaTime, ForceMode.Acceleration);
-			}
-			else if(Input.GetButton("Right"))
-			{
-				rigidbody.AddRelativeTorque(Vector3.up * rotation * Mathf.Max(0.6f, Input.GetAxis("Forward")) * Time.deltaTime, ForceMode.Acceleration);
-			}
+			onGround = true;
 		}
-		else if(Input.GetButton("Backward"))
+		else
 		{
-			rigidbody.AddRelativeForce(Vector3.back * speed * Time.deltaTime);
-			
-			if(Input.GetButton("Left"))
-			{
-				rigidbody.AddRelativeTorque(Vector3.up * rotation * Mathf.Max(0.6f, Input.GetAxis("Backward")) * Time.deltaTime, ForceMode.Acceleration);
-			}
-			else if(Input.GetButton("Right"))
-			{
-				rigidbody.AddRelativeTorque(Vector3.up * -rotation * Mathf.Max(0.6f, Input.GetAxis("Backward")) * Time.deltaTime, ForceMode.Acceleration);
-			}
+			onGround = false;
 		}
-		else //when not moving in a direction, take the rotation vector of the object, invert it and move it back over time. Take this vector, flatten the y value, figure out which direction it is pointing and scale it accordingly.
+		
+		
+		/*
+		//normal
+		Debug.DrawLine(hit.point, hit.point + hit.normal, Color.white);
+		
+		//straight down
+		//Debug.DrawLine(transform.position, hit.point, Color.black);
+		
+		//reflected
+		Debug.DrawLine(hit.point, transform.position + Vector3.Reflect(Vector3.down, hit.normal), Color.red);
+		*/
+		
+		
+		/*
+		//normal
+		Debug.DrawLine(hit.point, hit.point + hit.normal, Color.white);
+		
+		//straight forward
+		Debug.DrawLine(hit.point, transform.position + transform.forward, Color.black);
+		
+		//reflected
+		Debug.DrawLine(hit.point, transform.position + -Vector3.Reflect(transform.forward, hit.normal), Color.red);
+		*/
+		
+		if(onGround)
 		{
-			
+			if(Input.GetButton("Forward"))
+			{
+				rb.AddRelativeForce(Vector3.forward * speed * Time.deltaTime);
+				
+				if(Input.GetButton("Left"))
+				{
+					rb.AddRelativeTorque(Vector3.up * -rotation * Mathf.Max(0.6f, Input.GetAxis("Forward")) * Time.deltaTime, ForceMode.Acceleration);
+				}
+				else if(Input.GetButton("Right"))
+				{
+					rb.AddRelativeTorque(Vector3.up * rotation * Mathf.Max(0.6f, Input.GetAxis("Forward")) * Time.deltaTime, ForceMode.Acceleration);
+				}
+			}
+			else if(Input.GetButton("Backward"))
+			{
+				rb.AddRelativeForce(Vector3.back * speed * Time.deltaTime);
+				
+				if(Input.GetButton("Left"))
+				{
+					rb.AddRelativeTorque(Vector3.up * rotation * Mathf.Max(0.6f, Input.GetAxis("Backward")) * Time.deltaTime, ForceMode.Acceleration);
+				}
+				else if(Input.GetButton("Right"))
+				{
+					rb.AddRelativeTorque(Vector3.up * -rotation * Mathf.Max(0.6f, Input.GetAxis("Backward")) * Time.deltaTime, ForceMode.Acceleration);
+				}
+			}
+			else //when not moving in a direction, take the rotation vector of the object, invert it and move it back over time. Take this vector, flatten the y value, figure out which direction it is pointing and scale it accordingly.
+			{
+				// Code idea from http://thehiddensignal.com/unity-angle-of-sloped-ground-under-player/
+				
+				// So the whole idea here is that the cross product results in a vector that is directly perpendicular to vectors crossed.
+				
+				// This first finds a vector that is directly perpendicular to both the down vector and the surface normal. However, this point is perpendicular to the direction we want.
+				Vector3 perpendicular1 = Vector3.Cross(hit.normal, Vector3.down);
+				
+				// Using the normal and this new perpendicular vector, we can find the perpendicular vector to those, which points directly down the slope.
+				Vector3 slope = Vector3.Cross(perpendicular1, hit.normal);
+				
+				Debug.DrawLine(transform.position, transform.position + transform.forward, Color.red);
+				Debug.DrawLine(transform.position, transform.position + Quaternion.AngleAxis(90, transform.up) * transform.forward, Color.green);
+				Debug.DrawLine(transform.position, transform.position + -transform.forward, Color.blue);
+				Debug.DrawLine(transform.position, transform.position + Quaternion.AngleAxis(270, transform.up) * transform.forward, Color.yellow);
+				
+				//Debug.Log(Vector3.Angle(slope,transform.forward));
+				float slideAngle = Vector3.Angle(slope,transform.forward);
+				if(slideAngle <= 90)
+				{
+					slideScale = (90 - slideAngle)/90;
+				}
+				else if(slideAngle > 90 && slideAngle <= 180)
+				{
+					slideScale = -((90-slideAngle)/90);
+					
+				}
+				Debug.Log(slideScale);
+				
+				// slide downhill
+				rb.AddForce(slope.normalized * Time.deltaTime * 300f * slideScale);
+			}
 		}
 	}
 }
