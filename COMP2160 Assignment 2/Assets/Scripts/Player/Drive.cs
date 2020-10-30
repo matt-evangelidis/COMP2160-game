fa3830b061def785c93b0648ae8f8a72902a3b49
Cody's Main Laptop <codster761@gmail.com>
2020-10-30 23:04:08 +1100

    Implemented rigidbody movement for the car

--- COMP2160 Assignment 2/Assets/Scripts/Player/Drive.cs
+++ COMP2160 Assignment 2/Assets/Scripts/Player/Drive.cs
@@ -2,34 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Drive : MonoBehaviour
 {
-
     public float speed = 10f;
     public float rotation = 90f; //in degrees
+	
+	private float angle = 0f;
+	
+	private Rigidbody rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        float inputVelocity = Input.GetAxis("Vertical") * speed;
-        float inputRotation = Input.GetAxis("Horizontal") * rotation;
-
-        inputVelocity *= Time.deltaTime;
-        inputRotation *= Time.deltaTime;
-
-        transform.Translate(0, 0, inputVelocity);
-
-        if (inputVelocity != 0)//can only turn when the car is in motion
-        {
-            transform.Rotate(0, inputRotation, 0);
-        }
-
+		Debug.Log(Input.GetAxis("Forward"));
     }
+	
+	void FixedUpdate()
+	{	
+		if(Input.GetButton("Forward"))
+		{
+			rigidbody.AddRelativeForce(Vector3.forward * speed * Time.deltaTime);
+			
+			if(Input.GetButton("Left"))
+			{
+				rigidbody.AddRelativeTorque(Vector3.up * -rotation * Mathf.Max(0.6f, Input.GetAxis("Forward")) * Time.deltaTime, ForceMode.Acceleration);
+			}
+			else if(Input.GetButton("Right"))
+			{
+				rigidbody.AddRelativeTorque(Vector3.up * rotation * Mathf.Max(0.6f, Input.GetAxis("Forward")) * Time.deltaTime, ForceMode.Acceleration);
+			}
+		}
+		else if(Input.GetButton("Backward"))
+		{
+			rigidbody.AddRelativeForce(Vector3.back * speed * Time.deltaTime);
+			
+			if(Input.GetButton("Left"))
+			{
+				rigidbody.AddRelativeTorque(Vector3.up * -rotation * Mathf.Max(0.6f, Input.GetAxis("Backward")) * Time.deltaTime, ForceMode.Acceleration);
+			}
+			else if(Input.GetButton("Right"))
+			{
+				rigidbody.AddRelativeTorque(Vector3.up * rotation * Mathf.Max(0.6f, Input.GetAxis("Backward")) * Time.deltaTime, ForceMode.Acceleration);
+			}
+		}
+		else //when not moving in a direction, take the rotation vector of the object, invert it and move it back over time. Take this vector, flatten the y value, figure out which direction it is pointing and scale it accordingly.
+		{
+			
+		}
+	}
 }
