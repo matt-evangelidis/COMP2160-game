edcf0afc0f2255eb43c569bc4d23257c0f3f3235
Matthew Evangelidis <54091051+Notintheslightest@users.noreply.github.com>
2020-11-05 14:32:16 +1100

    Comments for Camera distance scaling by the player's velocity, with minimum and maximum distances

--- COMP2160 Assignment 2/Assets/Scripts/Camera/SmoothCamAlt.cs
+++ COMP2160 Assignment 2/Assets/Scripts/Camera/SmoothCamAlt.cs
@@ -8,7 +8,9 @@ public class SmoothCamAlt : MonoBehaviour
     public float smoothTime = 1f; //time it takes the camera to reach the target
     public Vector3 defaultDistance = new Vector3(0f, 0f, -2f); //the point the camera follows
     public Vector3 velocity = Vector3.zero; //velocity for SmoothDamp
+    public float minDistance = 0.15f;
     public float maxDistance = 1.5f;
+
 
     // Update is called once per frame
     void LateUpdate()
@@ -22,8 +24,12 @@ public class SmoothCamAlt : MonoBehaviour
 
         Vector3 toPos = target.position + (target.rotation * defaultDistance);
         
-        smoothTime = Mathf.Clamp(smoothTime * velocity.magnitude, 0.15f, maxDistance);
+        //smoothTime gets scaled by velocity.magnitude to increase the distance from the camera to the player as it accelerates
+        //smoothTime is clamped between minDistance and maxDistance
+        smoothTime = Mathf.Clamp(smoothTime * velocity.magnitude, minDistance, maxDistance);
         
+        //As the player accelerates, smoothTime will reach maxDistance, and smoothTime will be halved 
+        //to allow the camera to remain at a reasonable distance
         if (smoothTime == maxDistance)
         {
             smoothTime = maxDistance/2;
